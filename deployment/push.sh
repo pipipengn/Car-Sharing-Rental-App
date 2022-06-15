94c65be67fbfefa0da92afd8dd1407b3c709fc3e
Xinhui Yuan <pipipengn@gmail.com>
2022-06-15 01:52:23 -0700

    commit

--- deployment/push.sh
+++ deployment/push.sh
@@ -1,3 +1,4 @@
 DOMAIN=$1
-docker tag coolcar-"$DOMAIN":latest 402494663939.dkr.ecr.us-west-2.amazonaws.com/coolcar:"$DOMAIN"
-docker push 402494663939.dkr.ecr.us-west-2.amazonaws.com/coolcar:"$DOMAIN"
+Account=$2
+docker tag coolcar-"$DOMAIN":latest "$Account".dkr.ecr.us-west-2.amazonaws.com/coolcar:"$DOMAIN"
+docker push "$Account".dkr.ecr.us-west-2.amazonaws.com/coolcar:"$DOMAIN"
