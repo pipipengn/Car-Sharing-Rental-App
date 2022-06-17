354efd6713180093fba58155f0926517682ed99e
Xinhui Yuan <pipipengn@gmail.com>
2022-06-16 23:08:28 -0700

    commit

--- deployment/push.sh
+++ deployment/push.sh
@@ -1,5 +1,5 @@
 DOMAIN=$1
-
+TAG=$2
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/l4j6s3h2
-docker tag coolcar-"$DOMAIN":latest public.ecr.aws/l4j6s3h2/coolcar:"$DOMAIN"
-docker push public.ecr.aws/l4j6s3h2/coolcar:"$DOMAIN"
+docker tag coolcar-"$DOMAIN":latest public.ecr.aws/l4j6s3h2/coolcar:"$DOMAIN"-"$TAG"
+docker push public.ecr.aws/l4j6s3h2/coolcar:"$DOMAIN"-"$TAG"
