f3bf0bbe48ab42f43f57c6e33e4bd78e9756ed4c
Xinhui Yuan <pipipengn@gmail.com>
2022-06-19 14:16:48 -0700

    commit

--- server/blob/s3/front_test/index.js
+++ server/blob/s3/front_test/index.js
@@ -5,16 +5,13 @@ imageForm.addEventListener("submit", async e => {
     e.preventDefault()
     const file = imageInput.files[0]
 
-    // get secure url from our server
     const url = "https://coolcar.s3.us-west-2.amazonaws.com/62aec076c8f95a7b7da75ef0/62aee17a93de6700ed66771a?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAV3NUNFUB3ISTDRSK%2F20220619%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220619T084234Z&X-Amz-Expires=500&X-Amz-SignedHeaders=host&x-id=PutObject&X-Amz-Signature=3e37579056e165091256855d99c0fabdbdd7ec2b2931e756401fd22e6dd954a3"
 
 
-    // post image direct to s3
     await fetch(url, {
         method: "PUT",
         body: file
     })
-    // post request to my server with url
     const imageUrl = url.split("?")[0]
     console.log("imageUrl", imageUrl)
 })
